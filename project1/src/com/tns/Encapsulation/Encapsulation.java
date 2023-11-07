b115b6ab566610444e73b39a534054a5d4da2305
hp <hp@192.168.1.43>
2023-11-07 16:40:12 +0530

    COMMIT

---
+++ project1/src/com/tns/Encapsulation/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.tns.Encapsulation;
+
+public class Encapsulation {
+	public int serialno;
+	public String name;
+	public int age;
+
+public void show()
+{
+	System.out.println("serialno: " +serialno+ "\tname:" +name+  "\tage:" +age);
+}
+}
---
+++ project1/src/com/tns/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tns.Encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation obj = new Encapsulation();
+		obj.serialno=1;
+		obj.name="anu";
+		obj.age=20;
+		obj.show();
+		
+	}
+
+}
